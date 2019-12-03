cbee96435dc9fb83461f6df324c2cb0dcb7271e0
Marat <regpo55555@gmail.com>
2019-12-03 23:55:58 +0600

    Lifecycle hooks added

--- src/components/Krasovsky.vue
+++ src/components/Krasovsky.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="parameters">
-    <div>Малая полуось (полярный радиус): {{ a }} м</div>
-    <div>Большая полуось (экваториальный радиус): {{ b }} м</div>
-    <div>Малая полуось (полярный радиус): {{ a }} м</div>
-
-    <!-- <input v-model="msg" placeholder="a" /> -->
+    <div>Малая полуось (полярный радиус): {{ b }} м</div>
+    <div>Большая полуось (экваториальный радиус): {{ a }} м</div>
+    <div>e<sup>2</sup>: {{ e_sq }} м</div>
+    <div>
+      <span>B: </span>
+      <span><input id ='B' type = 'number' v-model="B_deg"/></span>°
+      <span><input id ='B' type = 'number' v-model="B_min"/></span>'
+      <span><input id ='B' type = 'number' v-model="B_sec"/></span>''
+      
+    </div>
+    <div>B в градусах: {{ b_total_deg }}</div>
+    <div>cos B: {{ Math.cos(b_total_deg*Math.PI/180) }}</div>
+    <!-- angle * (180 / Math.PI); -->
   </div>
 </template>
 
 <script>
 export default {
+  beforeMount: function() {
+    this.e_sq = (Math.pow(this.a, 2) - Math.pow(this.b, 2))/Math.pow(this.a, 2)
+  
+  },
+  beforeUpdate: function() {
+    this.b_total_deg = parseFloat(this.B_deg) + parseFloat(this.B_min/60) + parseFloat(this.B_sec/3600)
+  },
   data: function() {
     return {
-      a: 6356863,
-      b: 6378245
+      a: 6378245,
+      b: 6356863,
+      e_sq: '',
+      B_deg: '',
+      B_min: '',
+      B_sec: '',
+      b_total_deg: ''
     };
   }
 };
@@ -23,5 +43,14 @@ export default {
 <style scoped>
 #parameters {
   text-align: left;
+}
+#B {
+  margin-right: 5px;
+  max-width: 45px;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
